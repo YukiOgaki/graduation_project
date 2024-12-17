af0c47deacff214daf8fdc1a0a49748361285c2d
YukiOgaki <teamgift8@gmail.com>
2024-12-17 16:49:05 +0900

    地図APIをenvに格納

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+# .envファイルを読み込む
+load_dotenv()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    mapbox_access_token = os.getenv("MAPBOX_ACCESS_TOKEN")
+    return render_template("index.html", mapbox_access_token=mapbox_access_token)
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,23 +1,21 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3VrZW1hcnUiLCJhIjoiY200cXozYXNzMTlpcTJpcTI2MWtlN2l6eiJ9.cZxyiZgUbFVMH3KkMPuO2Q'; // APIキー
+mapboxgl.accessToken = mapboxAccessToken;
 
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation);
 
 function successLocation(position) {
-    const userLat = position.coords.latitude;   // 緯度
-    const userLng = position.coords.longitude;  // 経度
+    const userLat = position.coords.latitude;
+    const userLng = position.coords.longitude;
 
-    // 地図を表示する
     const map = new mapboxgl.Map({
-        container: 'map', // HTML内のdivのID
+        container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [userLng, userLat], // 現在地を中心に表示
+        center: [userLng, userLat],
         zoom: 14
     });
 
-    // ピン（マーカー）を現在地に立てる
     new mapboxgl.Marker()
         .setLngLat([userLng, userLat])
-        .setPopup(new mapboxgl.Popup().setText("あなたの現在地")) // ポップアップテキスト
+        .setPopup(new mapboxgl.Popup().setText("あなたの現在地"))
         .addTo(map);
 }
 
@@ -25,7 +23,6 @@ function errorLocation() {
     alert("現在地を取得できませんでした。位置情報を許可してください。");
 }
 
-// 現在地を更新するボタン
 document.getElementById("update-location").addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition(successLocation, errorLocation);
 });
